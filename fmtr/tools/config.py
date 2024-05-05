f0b1d28c5bca82ec398550ea57cf1dc53bee9509
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2024-05-05 16:12:44 +0100

    Add base tools, config and datatype tools

---
+++ fmtr/tools/config.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+
+from fmtr.tools.config_tools import ConfigClass
+
+
+class ToolsConfig(ConfigClass):
+    ENCODING = 'UTF-8'
+
+    LIBRARY_NAME = 'fmtr.tools'
+
+    DATE_FILENAME_FORMAT = '%Y-%m-%d'
+
+    DATETIME_FILENAME_FORMAT = f'{DATE_FILENAME_FORMAT}@%H-%M-%S'
+
+    DATETIME_NOW = datetime.utcnow()
+
+    DATETIME_NOW_STR = DATETIME_NOW.strftime(DATETIME_FILENAME_FORMAT)
+
+    SERIALIZATION_INDENT = 4
