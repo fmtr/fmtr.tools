5f10c48935d738b0b9f8058344e79380327a9023
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2024-05-19 18:31:05 +0100

    Add missing WSL platform doc/typing

--- fmtr/tools/platform_tools.py
+++ fmtr/tools/platform_tools.py
@@ -3,10 +3,10 @@ from functools import lru_cache
 
 
 @lru_cache
-def is_wsl():
+def is_wsl() -> bool:
     """
 
-
+    Determine whether the platform is WSL or native Linux.
 
     """
     uname_data = platform.uname()
