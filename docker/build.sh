f011cbc1082eeecd4611ac406a9f77dd3aaf4234
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2024-07-22 11:40:57 +0100

    Add production Dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -1,2 +1,2 @@
-export TAG=dev
+export TAG=prod
 docker buildx build --file Dockerfile.${TAG} --secret id=HF_TOKEN,env=HF_TOKEN --progress=plain --platform linux/amd64 --load --tag fmtr.tools:${TAG} --build-context tools=../ .
