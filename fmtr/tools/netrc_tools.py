37a9b5cbe6f956c3dee7dd7d61a981e03b3067a7
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2024-08-02 07:58:22 +0100

    Fix tinynetrc being incapable of creating .netrc

--- fmtr/tools/netrc_tools.py
+++ fmtr/tools/netrc_tools.py
@@ -6,6 +6,17 @@ LOGIN = 'login'
 PASSWORD = 'password'
 
 
+class Netrc(Netrc):
+
+    def __init__(self, file=None):
+        if not file:
+            from pathlib import Path
+
+            path = Path.home() / ".netrc"
+            path.touch(exist_ok=True)
+
+        super().__init__(file=file)
+
 @lru_cache
 def get():
     return Netrc()
