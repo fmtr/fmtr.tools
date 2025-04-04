0c68ed4a6e914d517264124b0f162b1fa2059859
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-04-04 15:08:24 +0100

    Configure Logfire via injected OTEL environment variables

--- fmtr/tools/logging_tools.py
+++ fmtr/tools/logging_tools.py
@@ -1,10 +1,23 @@
 import logging
+import os
+
+from fmtr.tools import environment_tools
+
+DEVELOPMENT = "development"
+PRODUCTION = "production"
+HOST_DEFAULT = "log.sv.fmtr.dev"
+ORG_DEFAULT = "fmtr"
+STREAM_DEFAULT = DEVELOPMENT
+ENVIRONMENT_DEFAULT = DEVELOPMENT
+
+LEVEL_DEFAULT = logging.DEBUG if environment_tools.IS_DEBUG else logging.INFO
 
 
-def get_logger(name, version, level, environment):
+def get_logger(name, version, host=HOST_DEFAULT, org=ORG_DEFAULT, stream=STREAM_DEFAULT,
+               environment=ENVIRONMENT_DEFAULT, level=LEVEL_DEFAULT):
     """
 
-    Get a pre-configured loguru logger, if dependency is present, otherwise default to native logger.
+    Get a pre-configured logfire logger, if dependency is present, otherwise default to native logger.
 
     """
 
@@ -17,6 +30,14 @@ def get_logger(name, version, level, environment):
 
         return logger
 
+    key = environment_tools.get("FMTR_OBS_API_KEY")
+    traces_endpoint = f"https://{host}/api/{org}/v1/traces"
+    headers = f"Authorization=Basic {key},stream-name={stream}"
+
+    os.environ["OTEL_EXPORTER_OTLP_TRACES_ENDPOINT"] = traces_endpoint
+    os.environ["OTEL_EXPORTER_OTLP_HEADERS"] = headers
+    os.environ["OTEL_EXPORTER_OTLP_INSECURE"] = str(False).lower()
+
     logfire.configure(
         service_name=name,
         service_version=version,
@@ -27,9 +48,10 @@ def get_logger(name, version, level, environment):
 
     logging.getLogger(name).setLevel(level)
 
-    return logfire
+    logger = logfire
+    return logger
 
 
-logger = get_logger(name='fmtr.tools', version='0.0.0', level=logging.DEBUG, environment='dev')
+logger = get_logger(name='fmtr.tools', version='0.0.0')
 
 logger = get_logger()
