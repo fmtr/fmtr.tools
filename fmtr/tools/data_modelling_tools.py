2ffd5a4fe7652203bdd5312b3a82604ec75ec087
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-04-21 11:52:07 +0100

    Add tabular/DataFrame methods to data modelling tools

--- fmtr/tools/data_modelling_tools.py
+++ fmtr/tools/data_modelling_tools.py
@@ -1,6 +1,18 @@
 from pydantic import BaseModel, RootModel
 
 
+def to_df(*objs):
+    """
+
+    DataFrame representation of Data Models as rows.
+
+    """
+    from fmtr.tools import tabular
+    rows = [obj.model_dump() for obj in objs]
+    df = tabular.DataFrame(rows)
+    return df
+
+
 class MixinFromJson:
 
     @classmethod
@@ -27,7 +39,22 @@ class Base(BaseModel, MixinFromJson):
     Base model
 
     """
-    ...
+
+    def to_df(self):
+        """
+
+        DataFrame representation with Fields as rows.
+
+        """
+
+        objs = []
+        for name in self.model_fields.keys():
+            val = getattr(self, name)
+            objs.append(val)
+
+        df = to_df(*objs)
+        df['id'] = list(self.model_fields.keys())
+        return df
 
 
 class Root(RootModel, MixinFromJson):
@@ -36,4 +63,12 @@ class Root(RootModel, MixinFromJson):
     Root (list) model
 
     """
-    ...
+
+    def to_df(self):
+        """
+
+        DataFrame representation with items as rows.
+
+        """
+
+        return to_df(*self.items)
