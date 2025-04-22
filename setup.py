06e94014126d92d1b7a7c483c552e3131c1244da
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-04-22 09:29:51 +0100

    Replace setup.py with template/generic version

--- setup.py
+++ setup.py
@@ -1,32 +1,46 @@
+from datetime import datetime
 from pathlib import Path
 from setuptools import find_namespace_packages, setup, find_packages
 
 import requirements
 
-name_ns, name_base = 'fmtr', 'tools'
-name = f'{name_ns}.{name_base}'
-path_segment = f'{name_ns}/{name_base}'
-path_base = Path(__file__).absolute().parent
-path_ver = path_base / path_segment / 'version'
-__version__ = path_ver.read_text().strip()
+ORG, PACKAGE = 'fmtr', 'tools'
+ORG_GITHUB = ORG
+AUTHOR = 'Frontmatter'
+AUTHOR_EMAIL = 'innovative.fowler@mask.pro.fmtr.dev'
+DESCRIPTION = 'Collection of high-level tools to simplify everyday development tasks, with a focus on AI/ML'
 
-packages = find_packages(where=path_segment)
-packages = [name] + [f'{name}.{nsp}' for nsp in packages]
+PATH_BASE = Path(__file__).absolute().parent
+IS_SINGLETON = (PATH_BASE / PACKAGE).exists()
+VERSION = 'version'
+
+if IS_SINGLETON:
+    packages = find_packages()
+    package_dir = None
+    name = PACKAGE
+    path_ver = PATH_BASE / name / VERSION
+else:
+    packages = find_namespace_packages()
+    package_dir = {'': '.'}
+    name = f'{ORG}.{PACKAGE}'
+    path_ver = PATH_BASE / ORG / PACKAGE / VERSION
+
+__version__ = path_ver.read_text().strip()
 
 setup(
     name=name,
     version=__version__,
-    url=f'https://github.com/{name_ns}/{name}',
-    license='Copyright © 2024 Frontmatter. All rights reserved.',
-    author='Frontmatter',
-    author_email='innovative.fowler@mask.pro.fmtr.dev',
-    description='Collection of high-level tools to simplify everyday development tasks, with a focus on AI/ML',
-    long_description=(path_base / 'README.md').read_text(),
+    url=f'https://github.com/{ORG_GITHUB}/{name}',
+    author=AUTHOR,
+    license=f'Copyright © {datetime.now().year} {AUTHOR}. All rights reserved.',
+    author_email=AUTHOR_EMAIL,
+    description=DESCRIPTION,
+    long_description=(PATH_BASE / 'README.md').read_text(),
     long_description_content_type='text/markdown',
-    packages=find_namespace_packages(),
-    package_dir={'': '.'},
+    packages=packages,
+    package_dir=package_dir,
     package_data={
-        name: [f'version'],
+        name: [VERSION],
     },
     entry_points={
         'console_scripts': requirements.CONSOLE_SCRIPTS,
