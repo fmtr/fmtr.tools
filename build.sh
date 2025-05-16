b43830f92bf04e0dd4e0503c508a59605292b6fa
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-05-16 12:22:27 +0100

    Split out compute CPU/GPU into compose overrides

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ export CHANNEL="dev"
 export ORG="fmtr"
 export PACKAGE="tools"
 export PORT="2200"
-export BASE="python"
+export BASE="pytorch"
 export VERSION=$(<"${ORG}/${PACKAGE}/version")
 
-if [ "$CHANNEL" = "dev" ]; then
-    export DOCKER_HOST=ssh://ed@ws.lan
-else
-    export DOCKER_HOST=ssh://ed@compute.lan
-fi
+export HOST="compute"
+export COMPUTE="gpu"
+
+export DOCKER_HOST=ssh://ed@${HOST}.lan
 
 docker compose build
-docker compose --project-name "${ORG}"-"${PACKAGE}" up --detach
+#
+docker compose --file compose.yml --file compose.${COMPUTE}.yml --project-name "${ORG}"-"${PACKAGE}" up --detach --force-recreate
