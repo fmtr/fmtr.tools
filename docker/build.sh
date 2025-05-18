823e14596b80f0b1690fced76b16c7f1ba5fc2ab
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-05-18 10:58:47 +0100

    Remove old docker files

--- docker/build.sh
+++
@@ -1,2 +0,0 @@
-export TAG=dev
-docker buildx build --file Dockerfile.${TAG} --secret id=HF_TOKEN,env=HF_TOKEN --progress=plain --platform linux/amd64 --load --tag fmtr.tools:${TAG} --build-context tools=../ .
