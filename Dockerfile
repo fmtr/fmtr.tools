b7b5941a3390260c9d209953b8530959e802708d
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-05-18 09:32:46 +0100

    Use docker volume for /opt/dev caching and fix permissions during build

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,18 @@ FROM fmtr/${BASE} AS dev
 
 ARG ORG
 ARG PACKAGE
+
+USER user
 WORKDIR /opt/dev/repo/${ORG}.${PACKAGE}
 
-COPY --from=package requirements.py .
+COPY --from=package --chown=user:user requirements.py .
 
 ARG EXTRAS
 RUN python requirements.py ${EXTRAS} > requirements.txt
 RUN pip install -r requirements.txt --no-cache-dir
+WORKDIR /
 
 FROM base AS prod
-COPY --from=package .. .
+COPY --from=package --chown=user:user  .. .
 RUN pip install . --no-cache-dir
+WORKDIR /
--- build.sh
+++ build.sh
@@ -5,13 +5,17 @@ export CHANNEL="dev"
 export ORG="fmtr"
 export PACKAGE="tools"
 export PORT="2200"
-export BASE="pytorch"
-export HOST="compute"
-export COMPUTE="gpu"
+export BASE="python"
+export HOST="ws"
+export COMPUTE="cpu"
 export EXTRAS="pdf,logging,debug,ai.client,sets"
 
 export VERSION=$(<"${ORG}/${PACKAGE}/version")
 export DOCKER_HOST=ssh://ed@${HOST}.lan
 
+docker compose --project-name "${ORG}-${PACKAGE}" down
+docker volume rm ${ORG}.${PACKAGE}
+
+docker pull fmtr/${BASE}
 docker compose build
 docker compose --file compose.yml --file compose.${COMPUTE}.yml --project-name "${ORG}"-"${PACKAGE}" up --detach --force-recreate
