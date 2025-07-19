1e7e5f2d50f31d6bea62c11dbedb021f2d1b9695
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-07-19 11:01:27 +0100

    Implement token refreshing in Google API tools

--- fmtr/tools/google_api_tools.py
+++ fmtr/tools/google_api_tools.py
@@ -1,3 +1,4 @@
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
@@ -20,8 +21,12 @@ class Authenticator:
 
     @classmethod
     def auth(cls):
-        msg = f'Doing auth for service {cls.SERVICE} ({cls.VERSION})...'
-        logger.info(msg)
+        """
+
+        Do initial authentication or refresh token if expired.
+
+        """
+        logger.info(f'Doing auth for service {cls.SERVICE} ({cls.VERSION})...')
 
         PATH_CREDS = cls.PATH / 'credentials.json'
         PATH_TOKEN = cls.PATH / f'{cls.SERVICE}.json'
@@ -29,9 +34,15 @@ class Authenticator:
         if PATH_TOKEN.exists():
             data_token = PATH_TOKEN.read_json()
             credentials = Credentials.from_authorized_user_info(data_token, cls.SCOPES)
+            if credentials.expired:
+                with logger.span(f'Credentials expired for {cls.SERVICE}. Will refresh...'):
+                    logger.warning(f'{cls.SERVICE}. {PATH_CREDS.exists()=} {PATH_TOKEN.exists()=} {credentials.valid=} {credentials.expired=} {credentials.expiry=}')
+                    credentials.refresh(Request())
+                    PATH_TOKEN.write_text(credentials.to_json())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(PATH_CREDS, cls.SCOPES)
+            flow.authorization_url(prompt='consent', access_type='offline')
             credentials = flow.run_local_server(open_browser=False, port=cls.PORT)
             PATH_TOKEN.write_text(credentials.to_json())
-        service = build(cls.SERVICE, cls.VERSION, credentials=credentials)
-        return service
+
+        return build(cls.SERVICE, cls.VERSION, credentials=credentials)
