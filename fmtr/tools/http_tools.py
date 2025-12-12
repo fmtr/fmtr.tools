b45df444c54817415e7624e52d4894ddf9a41f0e
Edward Brown <innovative.fowler@mask.pro.fmtr.dev>
2025-12-12 12:27:42 +0000

    Fix default http client not retrying for post, extend timrout

--- fmtr/tools/http_tools.py
+++ fmtr/tools/http_tools.py
@@ -1,5 +1,6 @@
 import httpx
-from httpx_retries import RetryTransport
+from functools import cached_property
+from httpx_retries import RetryTransport, Retry
 
 from fmtr.tools import logging_tools
 
@@ -13,14 +14,39 @@ class Client(httpx.Client):
 
     """
 
-    TRANSPORT = RetryTransport()
+    TIMEOUT = 10
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, transport=self.TRANSPORT, **kwargs)
+        super().__init__(*args, transport=self.transport, timeout=self.TIMEOUT, **kwargs)
+
+    @cached_property
+    def transport(self) -> RetryTransport:
+        """
+
+        Default Transport with retry
+
+        """
+        return RetryTransport(
+            retry=self.retry
+        )
+
+    @cached_property
+    def retry(self) -> Retry:
+        """
+
+        Default Retry
+
+        """
+        return Retry(
+            allowed_methods=Retry.RETRYABLE_METHODS,
+            backoff_factor=1.0
+        )
 
 
 client = Client()
 
 if __name__ == '__main__':
-    resp = client.get('http://httpbin.org/delay/10')
+    resp = client.get('https://postman-echo.com/delay/5')
+    resp.raise_for_status()
+    print(resp.json())
     resp
